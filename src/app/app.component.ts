83d9ea18142d1d458f138c0c78e85482709a7fdc
SamyBEKKA <samy.bekka@hotmail.com>
2024-10-06 11:22:07 +0200

    suppression de la nav bar de home avc la mise à jour de la nav général fonctionnel du Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
+import { CommandeComponent } from "./commande/commande.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FooterComponent],
+  imports: [RouterOutlet, CommonModule, FooterComponent, CommandeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
